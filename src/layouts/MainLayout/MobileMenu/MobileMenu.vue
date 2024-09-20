ed1d167fd2ac6f3bdc457546c4c25e01a848511e
Valerian Dwi Purnomo <valerian.purnomo@inticorp.tech>
2024-09-20 22:00:34 +0700

    Fixing accesbitilites on navbar

--- src/layouts/MainLayout/MobileMenu/MobileMenu.vue
+++ src/layouts/MainLayout/MobileMenu/MobileMenu.vue
@@ -1,11 +1,12 @@
 <template>
-  <div role="menu">
+  <div role="dialog" aria-modal="true">
     <!-- Backdrop with fade-in animation -->
     <div
       v-if="isDrawerOpen"
       class="fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-300"
       :class="{ 'opacity-0': !isDrawerOpen, 'opacity-100': isDrawerOpen }"
       @click="closeDrawer"
+      aria-hidden="true"
     ></div>
 
     <!-- Drawer with slide-in animation -->
@@ -20,9 +21,10 @@
       <div class="fixed top-8 right-8 justify-end">
         <img
           src="@/assets/icons/icon-menu-close.svg"
-          alt="icon-menu"
+          alt="Close menu"
           class="cursor-pointer"
           @click="closeDrawer()"
+          role="button"
         />
       </div>
       <div class="h-fit flex items-start flex-col space-y-6">
@@ -33,12 +35,15 @@
           :to="route.path"
           activeClass="!text-primary-color"
           @click="closeDrawer()"
-          >{{ route.linkName }}</RouterLink
+          role="menuitem"
         >
+          {{ route.linkName }}
+        </RouterLink>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
 import { routes } from "@/routes/route";
 // eslint-disable-next-line no-undef
--- src/layouts/MainLayout/NavBar/NavBar.vue
+++ src/layouts/MainLayout/NavBar/NavBar.vue
@@ -5,14 +5,14 @@
   >
     <img src="@/assets/logo.svg" alt="logo" role="img" />
     <nav role="navigation">
-      <div class="flex justify-end text-lg items-center" role="none">
+      <div class="flex justify-end text-lg items-center">
         <RouterLink
           v-for="route in routes"
           class="hover:text-primary-color text-secondary-color text-lg cursor-pointer px-6"
           :key="route.path"
           :to="route.path"
           activeClass="!text-primary-color"
-          role="menu"
+          role="menuitem"
         >
           {{ route.linkName }}
         </RouterLink>
@@ -20,7 +20,7 @@
     </nav>
   </header>
 </template>
+
 <script setup>
 import { routes } from "@/routes/route";
 </script>
-<style></style>
