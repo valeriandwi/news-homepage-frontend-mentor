477494a0036c56580eda4e8542f0eda0c93d6386
Valerian Dwi Purnomo <valerian.purnomo@inticorp.tech>
2024-09-20 21:54:44 +0700

    Fixing accesbitilites on navbar

--- src/layouts/MainLayout/MobileNavBar/MobileNavBar.vue
+++ src/layouts/MainLayout/MobileNavBar/MobileNavBar.vue
@@ -1,11 +1,11 @@
 <template>
   <header
     class="w-full bg-white h-12 mb-6 flex justify-between items-center"
-    role="menu"
+    role="navigation"
   >
-    <img src="@/assets/logo.svg" alt="logo" />
-    <div class="cursor-pointer" @click="onDrawerOpen">
-      <img src="@/assets/icons/icon-menu.svg" alt="icon-menu" />
+    <img src="@/assets/logo.svg" alt="logo" role="img" />
+    <div class="cursor-pointer" @click="onDrawerOpen" role="button">
+      <img src="@/assets/icons/icon-menu.svg" alt="icon-menu" role="img" />
     </div>
   </header>
 </template>
--- src/layouts/MainLayout/NavBar/NavBar.vue
+++ src/layouts/MainLayout/NavBar/NavBar.vue
@@ -3,15 +3,16 @@
     class="w-full bg-white h-12 flex justify-between items-center"
     role="navigation"
   >
-    <img src="@/assets/logo.svg" alt="logo" />
-    <nav>
-      <div class="flex justify-end text-lg items-center">
+    <img src="@/assets/logo.svg" alt="logo" role="img" />
+    <nav role="navigation">
+      <div class="flex justify-end text-lg items-center" role="none">
         <RouterLink
           v-for="route in routes"
           class="hover:text-primary-color text-secondary-color text-lg cursor-pointer px-6"
           :key="route.path"
           :to="route.path"
           activeClass="!text-primary-color"
+          role="menu"
         >
           {{ route.linkName }}
         </RouterLink>
